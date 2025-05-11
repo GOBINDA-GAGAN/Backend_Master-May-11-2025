30946ae7aaa08dbbf2b12a0d12bbb2e3e0ec7035
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-05-11 19:38:15 +0530

    make a api

--- Express Js/Express 1/index.js
+++ Express Js/Express 1/index.js
@@ -1,12 +1,23 @@
 import express from "express";
 
+import { User } from "./Data/User.js";
 const app = express();
 
 const Port = 5000;
 
+// GET Fetch data from the server
 app.get("/", (req, res) => {
-  res.send(`<h1> hello Backend</h1>`);
+  res.status(200).json({
+    massage: "Hello world",
+  });
+  res.send(`<h1> Hello Backend</h1>`);
 });
+
+// Industry Stander
+app.get("/api/v1/users", (req, res) => {
+  res.status(200).send(User);
+});
+
 
 app.listen(Port, () => {
   console.log("server Start");
