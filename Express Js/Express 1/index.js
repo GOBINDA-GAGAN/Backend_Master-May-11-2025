49f2b07e57de899bf28be875410ffccb4dfadc75
Gobinda Gagan Dey <gobindagagandey@gmail.com>
2025-05-11 19:44:09 +0530

    query parameter

--- Express Js/Express 1/index.js
+++ Express Js/Express 1/index.js
@@ -13,8 +13,13 @@ app.get("/", (req, res) => {
   res.send(`<h1> Hello Backend</h1>`);
 });
 
-// Industry Stander
+// Industry Stander 
+
 app.get("/api/v1/users", (req, res) => {
+
+  console.log(req.query);  // query parameter
+
+  
   res.status(200).send(User);
 });
 
